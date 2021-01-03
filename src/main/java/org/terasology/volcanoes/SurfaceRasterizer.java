d8216bfa22be5276a5ea0250f4ebd2dd715cea10
Cervator <cervator@gmail.com>
2021-01-03 13:13:31 -0600

    chore: address code review, extend version range accepted for CoreWorlds dependency

--- src/main/java/org/terasology/volcanoes/SurfaceRasterizer.java
+++ src/main/java/org/terasology/volcanoes/SurfaceRasterizer.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2020 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.volcanoes;
 
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
-import org.terasology.math.ChunkMath;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.Chunks;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldRasterizer;
@@ -41,10 +28,11 @@ public class SurfaceRasterizer implements WorldRasterizer {
     @Override
     public void generateChunk(CoreChunk chunk, Region chunkRegion) {
         ElevationFacet elevationFacet = chunkRegion.getFacet(ElevationFacet.class);
+        Vector3i tempVector = new Vector3i(); // Reuse same Vector3i object for optimization, not permanently stored
         for (Vector3ic position : chunkRegion.getRegion()) {
             float surfaceHeight = elevationFacet.getWorld(position.x(), position.z());
             if (position.y() < surfaceHeight) {
-                chunk.setBlock(ChunkMath.calcRelativeBlockPos(position, new Vector3i()), stone);
+                chunk.setBlock(Chunks.toRelative(position, tempVector), stone);
             }
         }
     }
