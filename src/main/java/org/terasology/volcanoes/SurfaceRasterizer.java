235561164907b427e8d98cfdeeed24174690a063
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2021-03-24 15:15:58 +0000

    Update for engine PR #4600

--- src/main/java/org/terasology/volcanoes/SurfaceRasterizer.java
+++ src/main/java/org/terasology/volcanoes/SurfaceRasterizer.java
@@ -8,8 +8,8 @@ import org.terasology.engine.registry.CoreRegistry;
 import org.terasology.engine.registry.In;
 import org.terasology.engine.world.block.Block;
 import org.terasology.engine.world.block.BlockManager;
+import org.terasology.engine.world.chunks.Chunk;
 import org.terasology.engine.world.chunks.Chunks;
-import org.terasology.engine.world.chunks.CoreChunk;
 import org.terasology.engine.world.generation.Region;
 import org.terasology.engine.world.generation.WorldRasterizer;
 import org.terasology.engine.world.generation.facets.ElevationFacet;
@@ -26,7 +26,7 @@ public class SurfaceRasterizer implements WorldRasterizer {
     }
 
     @Override
-    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+    public void generateChunk(Chunk chunk, Region chunkRegion) {
         ElevationFacet elevationFacet = chunkRegion.getFacet(ElevationFacet.class);
         Vector3i tempVector = new Vector3i(); // Reuse same Vector3i object for optimization, not permanently stored
         for (Vector3ic position : chunkRegion.getRegion()) {
